540327c5f9f467d3543bc381b3de860b90d8bdd1
gabriele <sacchigabri@hotmail.com>
2022-03-11 15:16:48 -0500

    prod-issue-test

--- test.js
+++ test.js
@@ -1,3 +1,43 @@
-setTimeout(() => { window.location.href = 'https://mycatnamedweb.github.io/cute%20cat.jpg'; }, 10 * 1000);
+// setTimeout(() => { window.location.href = 'https://mycatnamedweb.github.io/cute%20cat.jpg'; }, 10 * 1000);
+// alert('pwned #2');
 
-alert('pwned #2');
+
+alert('setting LS with "toIntercept"');
+localStorage.setItem('toIntercept', 'someValue');
+
+
+// // On our side:
+
+// const KEY_TO_INTERCEPT = 'toIntercept'; // NeoGames_503_TFATDT
+// const setLsValueInParent = (value) => localStorage.setItem(KEY_TO_INTERCEPT, value);
+// const getLsValueInParent = () => localStorage.getItem(KEY_TO_INTERCEPT);
+
+// const interceptNeoLsCalls = () => {
+//   // apply only to iOS IL
+//   const isIl = window.appInfo && window.appInfo.region === 'il';
+//   const isIos = window.cordova && window.cordova.platformId === 'ios;
+//   if (!isIl || !isIos) return;
+//   // override LS setitem
+//   const originalLocalstorageSetItem = localStorage.setItem;
+//   localStorage.setItem = (...args) => {
+//     const [key, value] = args;
+//     if (key === KEY_TO_INTERCEPT) {
+//       alert('@@ setting intercepted value');
+//       setLsValueInParent(value);
+//       return;
+//     }
+//     originalLocalstorageSetItem(...args);
+//   };
+//   // override LS getitem
+//   const originalLocalstorageGetItem = localStorage.getItem;
+//   localStorage.getItem = (...args) => {
+//     const key = args[0];
+//     if (key === KEY_TO_INTERCEPT) {
+//       alert('@@ getting intercepted value');
+//       return getLsValueInParent();
+//     }
+//     return originalLocalstorageGetItem(...args);
+//   };  
+// };
+
+// document.addEventListener('appready', interceptNeoLsCalls);
